68dc08acbb3beb923ed050c4306d847e3e2807a5
whatyesoh <whatyesoh@gmail.com>
2023-11-16 17:21:37 +0000

    uodate

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
-this.document.getElementById("parallax").style.transform = "translateY("+window.scrollY+")";
 window.addEventListener("scroll", function() {
     const distanceY = window.scrollY;
     if(distanceY / this.document.documentElement.clientHeight <= 2) {    
-        this.document.getElementById("parallax").style.transform = "translate("+ (distanceY * -.5 - 17) +"px," + distanceY * 1 + "px)";
+        this.document.getElementById("parallax").style.transform = "translate("+ (distanceY * -.5 - 17) +"px," + this.window.scrollY + "px)";
+        this.document.getElementById("arrow").style.transform = "translateY("+ (distanceY * 1.1 - 17) +"px)";
+        this.document.getElementById("arrow").style.opacity = 1 - distanceY/500;
     }
 })
