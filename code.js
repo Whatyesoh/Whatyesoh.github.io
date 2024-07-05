bc8d1c8fbd140e0b57fa1cf3df33daa7cf1e945c
whatyesoh <whatyesoh@gmail.com>
2024-07-05 12:04:13 -0400

    Fixed Title Scroll

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", function() {
     const distanceY = window.scrollY;
     if(distanceY / this.document.documentElement.clientHeight <= 2) {    
-        this.document.getElementById("parallax").style.transform = "translate("+ (distanceY * -.5 - 17) +"px," + this.window.scrollY + "px)";
-        this.document.getElementById("arrow").style.transform = "translateY("+ (distanceY * 1.1 - 17) +"px)";
-        this.document.getElementById("arrow").style.opacity = 1 - distanceY/500;
+        this.document.getElementById("parallax").style.left = -distanceY * .05 + "vw";
+        this.document.getElementById("arrow").style.top = distanceY * .05 + 55 + "vh";
+        this.document.getElementById("arrow").style.opacity = 1 - distanceY/400;
     }
 })
